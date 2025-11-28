2564937b6c3a9de72628c3ac75af58474808dbc1
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2025-11-28 11:30:22 +0100

    fastpath ite factory

---
+++ gpsl-otf/src/main/java/gpsl/syntax/ite/FastPathIteFactory.java
@@ -0,0 +1,242 @@
+package gpsl.syntax.ite;
+
+import gpsl.syntax.hashcons.HashConsingFactory;
+import gpsl.syntax.model.Atom;
+import gpsl.syntax.model.Conditional;
+import gpsl.syntax.model.Expression;
+
+public class FastPathIteFactory extends HashConsingFactory {
+    public FastPathIteFactory() {
+        super();
+    }
+
+    @Override
+    public Expression atom(String value, String delimiter) {
+        return super.conditional(super.atom(value, delimiter), t(), f());
+    }
+
+    @Override
+    public Expression conditional(Expression condition, Expression thenClause, Expression elseClause) {
+        //ite(⊤, m, _) = m  -- true condition
+        if (condition == t()) return thenClause;
+        //ite(⊥, _, m) = m -- false condition
+        if (condition == f()) return elseClause;
+        //ite(_, m, m) = m -- identical branches
+        if (thenClause == elseClause) return thenClause;
+        //ite(c, ⊤, ⊥) = c -- condition identity
+        if (thenClause == t() && elseClause == f()) return condition;
+        // ite(f, f, g) = f ∨ g = ite(f, ⊤, g)
+        if (condition == thenClause) {
+            return conditional(condition, t(), elseClause);
+        }
+        // ite(f, g, f) = f ∧ g = ite(f, g, ⊥)
+        if (condition == elseClause) {
+            return conditional(condition, thenClause, f());
+        }
+
+        //I suppose that condition, then and else are oneOf(ITE | t | f)
+
+        //find the top variable, here we use hashcons order
+        int min = Integer.MAX_VALUE;
+        Atom top = null;
+        if (condition instanceof Conditional f) {
+            var vF = (Atom) f.condition();
+            var tF = orderIndex(vF);
+            min = tF;
+            top = vF;
+        }
+
+        if (thenClause instanceof Conditional g) {
+            var vG = (Atom) g.condition();
+            var tG = orderIndex(vG);
+            if (tG < min) {
+                min = tG;
+                top = vG;
+            }
+        }
+
+        if (elseClause instanceof Conditional h) {
+            var vH = (Atom) h.condition();
+            var tH = orderIndex(vH);
+            if (tH < min) {
+                min = tH;
+                top = vH;
+            }
+        }
+
+        var f1 = cofactor(condition, min, true);
+        var f0 = cofactor(condition, min, false);
+
+        var g1 = cofactor(thenClause, min, true);
+        var g0 = cofactor(thenClause, min, false);
+
+        var h1 = cofactor(elseClause, min, true);
+        var h0 = cofactor(elseClause, min, false);
+
+        //Shannon expansion
+        var t = conditional(f1, g1, h1);
+        var e = conditional(f0, g0, h0);
+        if (t == e) return t;
+
+        return super.conditional(top, t, e);
+    }
+
+    int orderIndex(Expression var) {
+        return intern.map().get(var).tag();
+    }
+
+    Expression cofactor(Expression node, int min, boolean polarity) {
+        //terminals are their own cofactors
+        if (node == t() || node == f()) return node;
+        var cond = (Conditional) node;
+        var v = (Atom) cond.condition();
+        var tag = intern.map().get(v).tag();
+        if (polarity) {
+            if (min < tag) {
+                return node;
+            } else if (min == tag) {
+                return cond.trueBranch();
+            }
+            throw new RuntimeException("Impossible");
+        }
+        if (min < tag) {
+            return node;
+        } else if (min == tag) {
+            return cond.falseBranch();
+        }
+        throw new RuntimeException("Impossible");
+    }
+
+    @Override
+    public Expression conjunction(String op, Expression left, Expression right) {
+        // ⊤ ∧ m = m
+        if (left == t()) return right;
+        // m ∧ ⊤ = m
+        if (right == t()) return left;
+        // ⊥ ∧ ⊥ = ⊥, ⊥ ∧ m = ⊥, m ∧ ⊥ = ⊥
+        if (left == f() || right == f()) return f();
+        // m ∧ m = m
+        if (left == right) return left;
+        // m ∧ n -> n ∧ m, where m.tag > n.tag -- normalize commutativity
+        if (orderIndex(right) < orderIndex(left)) {
+            return conjunction(op, right, left);
+        }
+        // m ∧ n = ite(m, n, ⊥)
+        return conditional(left, right, f());
+    }
+
+    @Override
+    public Expression disjunction(String op, Expression left, Expression right) {
+        // ⊤ ∨ ⊤ = ⊤, m ∨ ⊤ = ⊤, ⊤ ∨ m = ⊤
+        if (left == t() || right == t()) return t();
+        // ⊥ ∨ m = m
+        if (left == f()) return right;
+        // m ∨ ⊥ = m
+        if (right == f()) return left;
+        // m ∨ m = m
+        if (left == right) return left;
+        // m ∨ n -> n ∨ m, where m.tag > n.tag -- normalize commutativity
+        if (orderIndex(right) < orderIndex(left)) {
+            return disjunction(op, right, left);
+        }
+        // m ∨ n = ite(m, ⊤, n)
+        return conditional(left, t(), right);
+    }
+
+    @Override
+    public Expression negation(String op, Expression expression) {
+        //¬⊤ = ⊥
+        if (expression == t()) return f();
+        //¬⊥ = ⊤
+        if (expression == f()) return t();
+        // ¬¬m = m -- double negation elimination
+        if (expression instanceof Conditional(Expression condition, Expression l, Expression r) && l == f() && r == t()) {
+            return condition;
+        }
+
+        //¬f = ite(f, ⊥, ⊤)
+        return conditional(expression, f(), t());
+    }
+
+    @Override
+    public Expression exclusiveDisjunction(String op, Expression left, Expression right) {
+        // ⊥ ⊕ g = g
+        if (left == f()) return right;
+        // f ⊕ ⊥ = f
+        if (right == f()) return left;
+        // ⊤ ⊕ g = ¬g
+        if (left == t()) return not(right);
+        // f ⊕ ⊤ = ¬f
+        if (right == t()) return not(left);
+        // f ⊕ f = ⊥
+        if (left == right) return f();
+
+        // f ⊕ g = (f ∧ ¬g) ∨ (¬f ∧ g) = ite(f, ¬g, g)
+        return conditional(left, not(right), right);
+    }
+
+    @Override
+    public Expression implication(String op, Expression left, Expression right) {
+        // ⊥ → m = ⊤, m → ⊤ = ⊤
+        if (left == f() || right == t()) return t();
+        // ⊤ → m = m
+        if (left == t()) return right;
+        // m → ⊥ = ¬m
+        if (right == f()) return not(left);
+        // m → m = ⊤
+        if (left == right) return t();
+
+        // f → g = ¬f ∨ g = ite(f, g, ⊤)
+        return conditional(left, right, t());
+    }
+
+    @Override
+    public Expression equivalence(String op, Expression left, Expression right) {
+        // ⊤ ↔ g = g
+        if (left == t()) return right;
+        // f ↔ ⊤ = f
+        if (right == t()) return left;
+        // ⊥ ↔ g = ¬g
+        if (left == f()) return not(right);
+        // f ↔ ⊥ = ¬f
+        if (right == f()) return not(left);
+        // f ↔ f = ⊤
+        if (left == right) return t();
+
+        // f ↔ g = (f → g) ∧ (g → f) = ite(f, g, ¬g) # Same as XNOR
+        return conditional(left, right, not(right));
+    }
+
+    public Expression nand(Expression left, Expression right) {
+        // f ⊼ ⊥ = ⊤, ⊥ ⊼ g = ⊤
+        if (left == f() || right == f()) return t();
+        // ⊤ ⊼ g = ¬g
+        if (left == t()) return not(right);
+        // f ⊼ ⊤ = ¬f
+        if (right == t()) return not(left);
+        // f ⊼ f = ¬f
+        if (left == right) return not(left);
+
+        // f ⊼ g = ¬(f ∧ g) = ite(f, ¬g, true)
+        return conditional(left, not(right), t());
+    }
+
+    public Expression nor(Expression left, Expression right) {
+        // ⊤ ⊽ m = ⊥, m ⊽ ⊤ = ⊥
+        if (left == t() || right == t()) return f();
+        // ⊥ ⊽ g = ¬g
+        if (left == f()) return not(right);
+        // f ⊽ ⊥ = ¬f
+        if (right == f()) return not(left);
+        // f ⊽ f = ¬f
+        if (left == right) return not(left);
+
+        // f ⊽ g = ¬(f ∨ g) = ite(f, false, ¬g)
+        return conditional(left, f(), not(right));
+    }
+
+    public Expression xnor(Expression left, Expression right) {
+        // f ⊙ g = ¬(f ⊕ g) = ite(f, g, ¬g)  # Same as equiv!
+        return equivalence(left, right);
+    }
+}
